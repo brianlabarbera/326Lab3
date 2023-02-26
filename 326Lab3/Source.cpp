f6371b31f1d5aae3ceba2589af7e5c94f8b68389
Brian LaBarbera <labarberabrian00@gmail.com>
2023-02-26 14:31:41 -0500

    2/3 problems

--- 326Lab3/Source.cpp
+++ 326Lab3/Source.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 
-int reverseNum1(int num);
+int reverseNum(int num);
+int recursiveReverse(int num, int reversedNum);
 void Problem1();
+void Problem2();
+void Problem3();
 
 int main() {
 
 
-	Problem1();
+	Problem3();
 
 
 	return 0;
@@ -19,11 +22,95 @@ void Problem1() {
 	std::cout << "Enter an integer:" << std::endl;
 	std::cin >> num;
 
-	std::cout << reverseNum1(num);
+	std::cout << reverseNum(num);
 
 }
 
-int reverseNum1(int num) {
+void Problem2() {
+
+	int num = 0;
+
+	std::cout << "Enter an integer:" << std::endl;
+	std::cin >> num;
+
+	std::cout << recursiveReverse(num, 0);
+
+}
+
+void Problem3() {
+
+	int degree = 0;
+
+	std::cout << "Enter highest degree of the polynomial" << std::endl;
+	std::cin >> degree;
+
+	int* ptr = new int[degree + 1];
+
+	std::cout << "Enter numbers:" << std::endl;
+ 
+	for (int i = 0; i <= degree; i++) {
+
+		std::cin >> ptr[i];
+
+	}
+
+	std::cout << std::endl;
+
+	int tempDegree = degree;
+
+	bool firstNum = false;
+
+	for (int j = 0; j <= degree; j++) {
+        
+		if (ptr[j] == 0) {
+			
+			tempDegree--;
+            
+			continue;
+		}
+
+		if (firstNum) {
+			std::cout << " + ";
+		}
+
+		if (tempDegree > 1) {
+			
+			if (ptr[j] != 1) {
+
+				std::cout << ptr[j];
+
+			}
+
+			std::cout << "x^" << tempDegree;
+
+		}
+
+		else if (tempDegree == 1) {
+			
+			if (ptr[j] != 1) {
+
+				std::cout << ptr[j];
+
+			}
+
+			std::cout << "x";
+
+		 }
+
+		else {
+
+			std::cout << ptr[j];
+		}
+
+		tempDegree--;
+
+		firstNum = true;
+
+	}
+
+}
+
+int reverseNum(int num) {
 
 	int reversedNum = 0;
 
@@ -37,5 +124,21 @@ int reverseNum1(int num) {
 	}
 
 	return reversedNum;
+
+}
+
+int recursiveReverse(int num, int reversedNum) {
+
+	if (num == 0) {
+		return reversedNum;
+	}
+
+	int tempNum = num % 10;
+
+	reversedNum = reversedNum * 10 + tempNum;
+
+	num /= 10;
+
+	return recursiveReverse(num, reversedNum);
 
 }
